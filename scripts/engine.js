1e34d3d4d45413f952a384f68e0b449b4640d2fa
Ângelo Nuffer <angelonuffer@gmail.com>
2015-04-10 01:01:27 +0000

    Remove useless comments.

--- scripts/engine.js
+++ scripts/engine.js
@@ -12,12 +12,5 @@ var GameObject = function(position, speed, mass, image) {
 		this.position[0] += this.speed[0];
 		this.position[1] += this.speed[1];
 		console.log(this.speed);
-		//var dx = Math.abs(this.position[0] - other.position[0]);
-		//var dy = Math.abs(this.position[1] - other.position[1]);
-		//var d = Math.sqrt(dx * dx + dy * dy);
-		//this.speed[0] += G * this.mass * other.mass / (d * d);
-		//var G = 5;
-		//this.position = [this.position[0] + (G * this.mass * other.mass / (d * d)), this.position[1]];
-		//console.log(this.position);
 	};
 }
